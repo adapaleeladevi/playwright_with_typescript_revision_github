8a8ed1ec11c04d4cddeaf3991ed6c77e3e2e9281
adapaleeladevi <adapa.leeladevigithub@gmail.com>
2026-09-13 21:06:08 +0530

    last index

---
+++ LeelaPractice/lastindexof.ts
@@ -0,0 +1,17 @@
+
+let x=[10, 20, 30, 20, 40, 34, 67, 89, 20, 43, 21, 90, 20, 47];
+//console.log(x.lastIndexOf(20));
+
+// for(let i of x.keys())
+// {
+//     console.log("keys",i);
+// }
+
+// for(let i of x){
+//     console.log(i);
+// }
+
+let y="hello welcome to the world";
+console.log(y.indexOf("to"));
+
+
