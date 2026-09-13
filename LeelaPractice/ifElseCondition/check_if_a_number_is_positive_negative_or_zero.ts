06a8a5e371bbe547d54d60a7c68a214562d63969
adapaleeladevi <adapa.leeladevigithub@gmail.com>
2026-09-13 22:26:24 +0530

    check_if_a_number_is_positive_negative_or_zero

---
+++ LeelaPractice/ifElseCondition/check_if_a_number_is_positive_negative_or_zero.ts
@@ -0,0 +1,15 @@
+// check_if_a_number_is_positive_negative_or_zero
+
+function check_if_a_number_is_positive_negative_or_zero(num:number):void
+{
+    if(num>=0)
+    {
+        console.log("given number is postive");
+    }
+    else
+    {
+        console.log("given number is negative");
+    }
+}
+
+check_if_a_number_is_positive_negative_or_zero(-1);
