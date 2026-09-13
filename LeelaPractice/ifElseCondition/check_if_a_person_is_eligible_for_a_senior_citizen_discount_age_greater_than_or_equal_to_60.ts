b62cd000f889f5685d41a9d3f19ba88d3323a408
adapaleeladevi <adapa.leeladevigithub@gmail.com>
2026-09-13 22:37:50 +0530

    check_if_a_person_is_eligible_for_a_senior_citizen_discount_age_greater_than_or_equal_to_60

---
+++ LeelaPractice/ifElseCondition/check_if_a_person_is_eligible_for_a_senior_citizen_discount_age_greater_than_or_equal_to_60.ts
@@ -0,0 +1,15 @@
+// check_if_a_person_is_eligible_for_a_senior_citizen_discount_age_greater_than_or_equal_to_60
+
+function check_if_a_person_is_eligible_for_a_senior_citizen_discount_age_greater_than_or_equal_to_60(age:number):void
+{
+    if(age>=60)
+    {
+        console.log("Senior Citizen");
+    }
+    else
+    {
+        console.log("Not Senior Citizen");
+    }
+}
+
+check_if_a_person_is_eligible_for_a_senior_citizen_discount_age_greater_than_or_equal_to_60(59);
